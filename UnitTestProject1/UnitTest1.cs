7e253197b0fd6cd6f50937b5f38a4db0383a05c9
bewipib777@mailart.ws <bewipib777@mailart.ws>
2021-09-14 10:44:39 +0200

    1

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UnitTestFirst;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(Math.Round(Math.Sinh(60 * Math.PI / 180),4), Math.Round(UnitTestFirst.Program.sh(60*Math.PI/180),4));
+        }
+    }
+}
